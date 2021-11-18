f08bd92baff90d5e8abb61196178c63d6f50687d
Deepi-2212 <94621544+Deepi-2212@users.noreply.github.com>
2021-11-18 19:22:03 +0530

    Add files via upload

---
+++ Challenges/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.index, name='home'),
+    path('challenges/', views.challengeList, name='challenge-list'),
+    path('myChallenges/', views.hosterMyChallenges, name='my-challenges'),
+    path('myProposals/', views.innovatorMyProposals, name='my-proposals'),
+    path('challenge-detail/<int:id>/', views.challengeDetail, name='challenge-detail'),
+    path('challenge-submissions/<int:id>/', views.viewChallengeSubmission, name='view-submissions'),
+    path('review-submissions/<int:id>/', views.reviewSubmission, name='review-submissions'),
+    path('challenge-update/<int:id>/', views.challengeUpdate, name='challenge-update'),
+    path('challenge-delete/<int:id>/', views.challengeDetail, name='challenge-delete'),
+    path('create-challenge/', views.CreateChallenge, name='create-challenge'),
+    path('submitchallenge/<int:id>', views.challengeSubmission, name='challenge-submission'),
+
+]
